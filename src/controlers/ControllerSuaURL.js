32e599fd6b5ea6f8330b8b73b62c2b19dbdfb5d9
TKVogais <gvparanagua@gmail.com>
2023-10-21 21:00:42 -0300

    Versão 1.0.1

--- src/controlers/ControllerSuaURL.js
+++ src/controlers/ControllerSuaURL.js
@@ -1,6 +1,11 @@
 const VerificarHost = async (req, res, next) => {
     const host = req.headers.host
+    console.log("===========HEADERS============")
+    console.log(req.headers)
+    console.log("==============================")
+    console.log("===========HOST============")
     console.log(host)
+    console.log("==============================")
     if (host === "localhost:4000") {
         next()
     } else {

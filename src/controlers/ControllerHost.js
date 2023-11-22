07fb13e0eed215e458e82c8b45c5eccc3bd065ea
TKVogais <jgparanagua@gmail.com>
2023-11-22 10:58:03 -0300

    Versão 1.2.0

--- src/controlers/ControllerHost.js
+++ src/controlers/ControllerHost.js
@@ -1,6 +1,18 @@
 const VerificarHost = async (req, res, next) => {
-    const mode = "local"    
-    const host = mode == "nuvem" ? "https://suaurl.com" : "http://localhost:3000/"
+    const mode = "nuvem-dev"
+    let host = ""
+
+    switch (mode) {
+        case "nuvem-prod":
+            host = "https://suaurl.com"
+            break;
+        case "nuvem-dev":
+            host = "https://www.cherrysocial.com.br/"
+            break;
+        case "local":
+            host = "http://localhost:3000"
+            break;
+    }
     const referer = req.headers.referer
     if (referer.includes(host)) {
         next()

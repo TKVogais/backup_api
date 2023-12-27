b599b07dc69d132d832e47524170ed528b9c3e82
TKVogais <gvparanagua@gmail.com>
2023-12-26 23:07:13 -0300

    Versão 2.0.7

--- src/controlers/ControllerTracking.js
+++ src/controlers/ControllerTracking.js
@@ -1,26 +1,28 @@
 const geradorTokenConfirmacao = require("../utils/stringAleatoria")
-
+const requestIP = require('request-ip')
 
 const Redirecionamento = async (req, res) => {
-    const ipAddress = requestIP.getClientIp(req)
-    const ipSocket = req.socket.remoteAddress
-    const ipHeader = req.header('x-forwarded-for')
+    const IP = requestIP.getClientIp(req)
 
-    console.log(`Método 1 - IP: ${ipAddress}`)
-    console.log(`Método 2 - IP: ${ipSocket}`)
-    console.log(`Método 3 - IP: ${ipHeader}`)
+    if (!IP) {
+        return res.json({
+            status: 603,
+            VPN: true,
+            message: "IP diferente, mudança de dispositivo ou uso de VPN",
+            limite: false,
+            semRota: false,
+        })
+    }
 
     if (req.app.locals.rotas.length > 0) {
         const IPs = req.app.locals.IPs
         const idUsuario = req.body.idUsuario
-        const IP = req.body.IP
         const avatar = req.body.avatar
         const dificuldade = req.body.dificuldade
         const nome = req.body.usuario
         const limite = req.app.locals.limite
         const rotas = req.app.locals.rotas
         let valor = 0
-        console.log(`Usuário: ${nome} - IP: ${ipUsuario}`)
         switch (dificuldade) {
             case "facil": valor = 0.008; break;
             case "medio": valor = 0.010; break;
@@ -49,17 +51,17 @@ const Redirecionamento = async (req, res) => {
             tokenCriado = false
         }
         if (!usuario) {
-            // IPs.forEach((usuario) => {
-            //     if (usuario.IP == IP && usuario.idUsuario != idUsuario) {
-            //         return res.json({
-            //             status: 603,
-            //             VPN: true,
-            //             message: "Esse IP já está em uso!",
-            //             limite: false,
-            //             semRota: false,
-            //         })
-            //     }
-            // })
+            IPs.forEach((usuario) => {
+                if (usuario.IP == IP && usuario.idUsuario != idUsuario) {
+                    return res.json({
+                        status: 603,
+                        VPN: true,
+                        message: "Esse IP já está em uso!",
+                        limite: false,
+                        semRota: false,
+                    })
+                }
+            })
             req.app.locals.tracking.push({
                 idUsuario: idUsuario,
                 usuario: nome,
@@ -70,24 +72,24 @@ const Redirecionamento = async (req, res) => {
                 avatar: avatar,
                 IP: IP
             })
-            // req.app.locals.IPs.push({
-            //     idUsuario: idUsuario,
-            //     IP: IP
-            // })
+            req.app.locals.IPs.push({
+                idUsuario: idUsuario,
+                IP: IP
+            })
             location = req.app.locals.tracking.length - 1
         } else {
             let User = req.app.locals.tracking[location]
-            // if (User.IP != IP && User.IP != "") {
-            //     return res.json({
-            //         status: 603,
-            //         VPN: true,
-            //         message: "IP diferente, mudança de dispositivo ou uso de VPN",
-            //         limite: false,
-            //         semRota: false,
-            //     })
-            // } else {
-            //     req.app.locals.tracking[location].IP = IP
-            // }
+            if (User.IP != IP && User.IP != "") {
+                return res.json({
+                    status: 603,
+                    VPN: true,
+                    message: "IP diferente, mudança de dispositivo ou uso de VPN",
+                    limite: false,
+                    semRota: false,
+                })
+            } else {
+                req.app.locals.tracking[location].IP = IP
+            }
         }
         let nRota = req.app.locals.tracking[location].rota - 1
         let clicks = req.app.locals.tracking[location].clicks
